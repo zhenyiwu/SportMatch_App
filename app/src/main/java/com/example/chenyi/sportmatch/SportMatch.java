e44dfc736deb1bb06e65c1de7724f872b40ffe74
chenyi <wucy@locust.csie.ncku.edu.tw>
2017-08-13 01:48:01 +0800

    init commit

---
+++ app/src/main/java/com/example/chenyi/sportmatch/SportMatch.java
@@ -0,0 +1,13 @@
+package com.example.chenyi.sportmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SportMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sport_match);
+    }
+}
